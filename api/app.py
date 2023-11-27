e2c836462a38115fdf6a58b954d2aa71de6b6168
adelppi <adelppi.baig@gmail.com>
2023-11-27 13:40:29 +0900

    ランダム四字熟語適用した

--- api/app.py
+++ api/app.py
@@ -1,26 +1,36 @@
 from flask import Flask, jsonify
 import random
+import json
 
 app = Flask(__name__)
 
-# クイズの問題と回答を含むJSONデータ
-questions = [
-    {
-        "id": 1,
-        "question": "問題1！",
-        "answer": "False"
-    },
-    {
-        "id": 2,
-        "question": "問題2！",
-        "answer": "True"
-    }
-]
+def random_yozizyukugo():
+    yozizyukugo_path = './data/yozizyukugo.json'
+
+    with open(yozizyukugo_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    if isinstance(data, list):
+        selected_data = random.choice(data)
+        moji_value = selected_data.get('moji', '')
+        imi_value = selected_data.get('imi', '')
+    else:
+        moji_value = data.get('moji', '')
+        imi_value = data.get('imi', '')
+
+    quiz_sentence = f'「{moji_value}」の意味は\n「{imi_value}」である.\n'
+
+    return quiz_sentence
 
 @app.route('/oxquiz', methods=['GET'])
 def get_quiz():
-    random_question = random.choice(questions)
-    return jsonify(random_question)
+    random_question = random_yozizyukugo()
+    return jsonify(
+        {
+            "question": random_question,
+            "answer": "False"
+        }
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
